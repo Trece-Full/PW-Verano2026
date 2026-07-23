3840386e51b69003aee2631589b5a572fdb6062e
Ronaldo <xdoooorrr@gmail.com>
2026-07-23 16:51:34 -0700

    Vercel

--- crud_peliculas/script.js
+++ crud_peliculas/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/peliculas";
+const API_URL = "https://frontpeliculascrud.vercel.app/peliculas";
 
 const formulario = document.getElementById("formulario");
 const id = document.getElementById("id");
